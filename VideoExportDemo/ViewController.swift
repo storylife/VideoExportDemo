7cc158c717700703fc271f659addf9e5304c78b8
Oliver Brzoska <oliver@goodsquirrel.com>
2019-04-16 21:55:44 +0200

    Initial Commit

---
+++ VideoExportDemo/ViewController.swift
@@ -0,0 +1,14 @@
+//  Copyright © 2019 Olli Brzoska. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
